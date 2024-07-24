251088a2b13fad5b2ec7851088dfa8f951a170fd
Sondre Lindkjolen <sondredl@gmail.com>
2024-07-24 07:32:49 +0200

    calculates latitude

--- components/coordinateTransform/main.cpp
+++ components/coordinateTransform/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <filesystem>
 
+constexpr int ONE_DEGRE_OF_LATITUDE_IN_METERS = 111000;
+
 struct latLonPos
 {
   double Lat{};
@@ -24,13 +26,21 @@ double CalcDiffLon(double fromLon, double toLon)
   return fromLon - toLon;
 }
 
+double DegreesToMeters(double deltaDegrees)
+{
+  return deltaDegrees * ONE_DEGRE_OF_LATITUDE_IN_METERS;
+}
+
 int main()
 {
   latLonPos start{};
-  start.Lat = 0.0000;
+  start.Lat = 8.5748;
   start.Lon = 0.0100;
 
   latLonPos end{};
-  end.Lat = 0.0100;
+  end.Lat = 8.5747;
   end.Lon = 0.0000;
+
+  std::cout << "delta Lat degrees = " << CalcDiffLat(start.Lat, end.Lat) << std::endl;
+  std::cout << "delta Lat meters = " << degreesToMeters(CalcDiffLat(start.Lat, end.Lat)) << std::endl;
 }
