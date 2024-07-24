2f5ea9084bf6aa0f1546724ad3db2429d6ba3989
Sondre Lindkjolen <sondredl@gmail.com>
2024-07-24 08:22:25 +0200

    successfully calculates distance between two points, with considering lat degrees north

--- components/coordinateTransform/main.cpp
+++ components/coordinateTransform/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <filesystem>
+#include <math.h>
 
-constexpr int ONE_DEGRE_OF_LATITUDE_IN_METERS = 111000;
+// constexpr int ONE_DEGRE_OF_LATITUDE_IN_METERS = 111000;
+constexpr int ONE_DEGRE_OF_LATITUDE_IN_METERS = 111320;
 
 struct latLonPos
 {
@@ -16,12 +18,12 @@ struct metricVector
   double ZDelta;
 };
 
-double CalcDiffLat(double fromLat, double toLat)
+double CalcDiffLon(double fromLat, double toLat)
 {
   return fromLat - toLat;
 }
 
-double CalcDiffLon(double fromLon, double toLon)
+double CalcDiffLat(double fromLon, double toLon)
 {
   return fromLon - toLon;
 }
@@ -31,16 +33,38 @@ double DegreesToMeters(double deltaDegrees)
   return deltaDegrees * ONE_DEGRE_OF_LATITUDE_IN_METERS;
 }
 
+double DegreesToRadians(double degrees)
+{
+  return (degrees * (std::numbers::pi / 180));
+}
+
+double MetricDistanceGivenLat(double avgLatRad, double deltaLon)
+{
+  double distance = deltaLon * std::cos(avgLatRad) * ONE_DEGRE_OF_LATITUDE_IN_METERS;
+  std::cout << "delta Lon: " << deltaLon << std::endl;
+  std::cout << "cos(lat): " << std::cos(avgLatRad) << std::endl;
+  std::cout << "meters/degree: " << ONE_DEGRE_OF_LATITUDE_IN_METERS << std::endl;
+  return distance;
+}
+
 int main()
 {
   latLonPos start{};
-  start.Lat = 8.5748;
-  start.Lon = 0.0100;
+  start.Lat = 60.5902;
+  start.Lon = 8.5733;
 
   latLonPos end{};
-  end.Lat = 8.5747;
-  end.Lon = 0.0000;
+  end.Lat = 60.5902;
+  end.Lon = 8.5732;
 
-  std::cout << "delta Lat degrees = " << CalcDiffLat(start.Lat, end.Lat) << std::endl;
-  std::cout << "delta Lat meters = " << degreesToMeters(CalcDiffLat(start.Lat, end.Lat)) << std::endl;
+  auto diffLonDeg    = CalcDiffLon(start.Lon, end.Lon);
+  auto diffLonMeters = DegreesToMeters(diffLonDeg);
+
+  std::cout << "delta Lon degrees = " << diffLonDeg << std::endl;
+  std::cout << "delta Lon meters = " << diffLonMeters << std::endl;
+
+  // std::cout << "cos(60.5902): " << std::cos(DegreesToRadians(60.5902)) << std::endl;
+  auto distanceAtLatitude = MetricDistanceGivenLat(DegreesToRadians(start.Lat), diffLonDeg);
+  std::cout << "distance at lat: " << distanceAtLatitude << std::endl;
+  // auto radiansNorth = DegreesToRadians(start.Lat);
 }
