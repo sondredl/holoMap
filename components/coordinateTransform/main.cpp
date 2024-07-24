cd726f8f254e8f1b0b4311f3d58438c5e5ecdf5c
Sondre Lindkjolen <sondredl@gmail.com>
2024-07-24 06:25:54 +0200

    creates diff calculating functions

--- components/coordinateTransform/main.cpp
+++ components/coordinateTransform/main.cpp
@@ -7,6 +7,16 @@ struct latLonPos
   double lon{};
 };
 
+double calcDiffLat(double fromLat, double toLat)
+{
+  return fromLat - toLat;
+}
+
+double calcDiffLon(double fromLon, double toLon)
+{
+  return fromLon - toLon;
+}
+
 int main()
 {
   std::cout << "hello holoWorld" << std::endl;
